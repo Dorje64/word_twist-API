f4a7cc176840d0cc3a807ac1d7db537311efce3f
Dorje <rtorchiiw@gmail.com>
2019-12-03 22:28:51 +0545

    load vendor file

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,12 @@
 class WordsController < ApplicationController
   def select
+    words = {}
+    File.open("#{Rails.root}/vendor/Words/Words/en.txt") do |file|
+      file.each do |line|
+        words[line.strip] = true
+      end
+    end
+    # puts words
+    render json: words.first
   end
 end
